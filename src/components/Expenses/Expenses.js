bc637e4abcaf1047dc1094025d3004abcc3b0ad3
r.sadegh.f <yoriqiedtay356epkoafzi6eef2w6e55go47xw6pqjtjllhsp7va>
2022-05-05 18:10:59 +0200

    project structure

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,17 +1,18 @@
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import {generateUniqueID} from "web-vitals/dist/lib/generateUniqueID";
+import Card from "../UI/Card";
 
 const Expenses = (props) => {
 
 
     return (
-        <div className="expenses">
+        <Card className="expenses">
             {
                 props.expenses.map(expense => <ExpenseItem key={generateUniqueID()} title={expense.title} amount={expense.amount} date={expense.date} />)
             }
 
-        </div>
+        </Card>
     );
 };
 
